fbb6db28a442b21dd8ba7c0621104b1aef551454
Piotr Babel <piotr.babel@gmail.com>
2023-04-25 15:23:12 +0800

    Remove close_factor. Use external contract.

--- integration-tests/tests/test_oracles.rs
+++ integration-tests/tests/test_oracles.rs
@@ -1036,7 +1036,6 @@ fn setup_redbank(wasm: &Wasm<OsmosisTestApp>, signer: &SigningAccount) -> (Strin
             owner: signer.address(),
             config: CreateOrUpdateConfig {
                 address_provider: Some(addr_provider_addr.clone()),
-                close_factor: Some(Decimal::percent(10)),
             },
         },
     );
